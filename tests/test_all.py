12826e051914e458ad1af176d8a839669486bf5b
Johannes Koester <johannes.koester@uni-due.de>
2026-09-08 09:19:55 +0200

    comment out not completed test for now and activate pixi run test in CI

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,16 +1,15 @@
-import subprocess as sp
 
 
-def test_all():
-    for i in range(2):
-        print(f"Run {i}")
-        sp.run(
-            [
-                "kisiac",
-                "--non-interactive",
-                "update-hosts",
-                "--skip-system-upgrade",
-                "localhost",
-            ],
-            check=True,
-        )
+# def test_all():
+#     for i in range(2):
+#         print(f"Run {i}")
+#         sp.run(
+#             [
+#                 "kisiac",
+#                 "--non-interactive",
+#                 "update-hosts",
+#                 "--skip-system-upgrade",
+#                 "localhost",
+#             ],
+#             check=True,
+#         )
